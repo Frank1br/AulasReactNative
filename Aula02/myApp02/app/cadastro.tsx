671f19ba264015cc16cb6fb27a506e350fa9a7d2
Frank Oliveira <fm0325836@gmail.com>
2026-08-31 16:12:30 -0300

    feat: implement registration form with input fields and navigation to login

--- Aula02/myApp02/app/cadastro.tsx
+++ Aula02/myApp02/app/cadastro.tsx
@@ -1,10 +1,18 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
+
 
 export default function Login() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tela de cadastro</Text>
+        <TextInput placeholder="Seu Nome" style={styles.input}/>
+        <TextInput placeholder="emailmail@gmail.com" style={styles.input} keyboardType="email-address"/>
+        <TextInput placeholder="Senha" style={styles.input} secureTextEntry/>
+        <TouchableOpacity style={styles.buttonPrimary} onPress={() => router.navigate("/login")}>
+                  <Text style={styles.buttonPrimaryText}>Cadastrar</Text>
+        </TouchableOpacity>
         <Text>
              Ja tem login? {' '}
             <Link href="/login" style={styles.link}>
@@ -29,5 +37,27 @@ const styles = StyleSheet.create({
   link: {
     fontSize: 18,
     color: "blue",
+  },
+  input: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: .9,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: "80%",
+    borderRadius: 8,
+  },
+  buttonPrimary: {
+    backgroundColor: "#28A745", 
+    width: "80%",
+    paddingVertical: 14,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  buttonPrimaryText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
