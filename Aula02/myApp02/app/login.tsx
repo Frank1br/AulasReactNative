0dfa3094023d10bab6343df1a1c40e1710a20c76
Frank Oliveira <fm0325836@gmail.com>
2026-08-31 16:17:18 -0300

    feat: enhance login component with input fields and navigation

--- Aula02/myApp02/app/login.tsx
+++ Aula02/myApp02/app/login.tsx
@@ -1,18 +1,24 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
 
 export default function Login() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tela de Login</Text>
-        <Text>
-            Ja tem cadastro? {' '}
-            <Link href="/cadastro" style={styles.link}>
-                Cadastre-se
-            </Link>
-        </Text>
-    </View>
-  );
+    const router = useRouter();
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Tela de Login</Text>
+                <TextInput placeholder="emailmail@gmail.com" style={styles.input} keyboardType="email-address"/>
+                <TextInput placeholder="Senha" style={styles.input} secureTextEntry/>
+                <TouchableOpacity style={styles.buttonPrimary} onPress={() => router.navigate("/login")}>
+                        <Text style={styles.buttonPrimaryText}>Entrar</Text>
+                </TouchableOpacity>
+                <Text>
+                    Não tem um cadastro? {' '}
+                    <Link href="/cadastro" style={styles.link}>
+                        Cadastrar-se!
+                    </Link>
+                </Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -29,5 +35,27 @@ const styles = StyleSheet.create({
   link: {
     fontSize: 18,
     color: "blue",
+  },
+  input: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: .9,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: "80%",
+    borderRadius: 8,
+  },
+  buttonPrimary: {
+    backgroundColor: "#28A745", 
+    width: "80%",
+    paddingVertical: 14,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  buttonPrimaryText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
